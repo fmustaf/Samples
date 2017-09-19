e469d31ef071ca3e78ed74f15e439e55a0c155aa
Faisal Mustafa <fmustaf@hotmail.com>
2017-09-19 23:16:45 +0000

    Enter the comments for your changes

---
+++ eShopOnContainers-dev/src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,28 @@
+﻿using Basket.API.Infrastructure.Middlewares;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Microsoft.eShopOnContainers.Services.Basket.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseFailing(options =>
+                {
+                    options.ConfigPath = "/Failing";                   
+                })
+                .UseHealthChecks("/hc")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
